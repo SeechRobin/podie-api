4a657d65308b63d0b45a54f35c83166f849d45c3
Robin Mukanganise <seechrobin@gmail.com>
2020-10-18 09:51:43 +0100

    setup index home (#1)

---
+++ api/controllers/home/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  friendlyName: 'Index',
+  description: 'Index home.',
+  inputs: {
+
+  },
+  exits: {
+
+  },
+
+  fn: async function (_, exits) {
+
+    // All done.
+    exits.success({message: 'Podie Sails API'});
+
+  }
+};
